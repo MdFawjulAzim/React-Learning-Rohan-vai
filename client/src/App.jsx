c86c7390a5b8b61be64f8383e6e6aa5a975d4d6a
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-07-15 22:07:31 +0600

    Default Route Hit 404 Page Show Route Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SingUp from "./pages/SingUp";
@@ -7,29 +14,55 @@ import OrganizerContent from "./components/Dashboard/content/OrganizerContent";
 import AdminContent from "./components/Dashboard/content/AdminContent";
 import UserContent from "./components/Dashboard/content/UserContent";
 import DashboardLayout from "./layout/DashboardLayout";
+import ErrorPage from "./components/Error/ErrorPage";
+
+function AppRoutes() {
+  const role = useSelector((state) => state.Auth.role);
+  const location = useLocation();
+
+  if (
+    role &&
+    (location.pathname === "/login" || location.pathname === "/signup")
+  ) {
+    return <Navigate to="/" replace />;
+  }
+
+  return (
+    <Routes>
+      <Route path="*" element={<ErrorPage />} />
+      <Route path="/" element={<HomePage />} />
+
+      {role ? (
+        <>
+          <Route path="/login" element={<HomePage />} />
+          <Route path="/signup" element={<HomePage />} />
+
+          <Route path="/dashboard" element={<DashboardLayout />}>
+            <Route element={<ProtectionRouter allowRole="admin" />}>
+              <Route path="admin" element={<AdminContent />} />
+            </Route>
+            <Route element={<ProtectionRouter allowRole="user" />}>
+              <Route path="user" element={<UserContent />} />
+            </Route>
+            <Route element={<ProtectionRouter allowRole="organizer" />}>
+              <Route path="organizer" element={<OrganizerContent />} />
+            </Route>
+          </Route>
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SingUp />} />
+        </>
+      )}
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SingUp />} />
-
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route element={<ProtectionRouter allowRole="admin" />}>
-            <Route path="admin" element={<AdminContent />} />
-          </Route>
-
-          <Route element={<ProtectionRouter allowRole="user" />}>
-            <Route path="user" element={<UserContent />} />
-          </Route>
-
-          <Route element={<ProtectionRouter allowRole="organizer" />}>
-            <Route path="organizer" element={<OrganizerContent />} />
-          </Route>
-        </Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Error/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="items-center justify-center w-full h-screen">
+      <h1 className="text-5xl text-red-500 ">Error Page</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
